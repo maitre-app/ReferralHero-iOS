0f1e3d5328605bf7f2bef17bb1f6f0352b1913b0
yesha patel <yp.patel30@gmail.com>
2023-07-02 22:11:55 +0530

    SPM Added

---
+++ Sources/ReferralHero-iOS/APIManager/APIManager.swift
@@ -0,0 +1,280 @@
+//
+//  APIManager.swift
+//  ReferralHero
+//
+//  Created by YESHA PATEL on 16/05/2023.
+//
+
+import Foundation
+
+public let RH = API_HELPER.shared
+
+public class API_HELPER
+{
+    private init() { }
+    static let shared = API_HELPER()
+    
+    public weak var delegate: RHDelegate?
+    
+    //MARK: - EndPoints -
+    let subscribers = "/subscribers"
+    let leaderboard = "/leaderboard"
+    
+    internal func storeUserData(response: Data?) {
+        if let data = response
+        {
+            UserDefaults.standard.set(data, forKey: "User")
+            UserDefaults.standard.synchronize()
+            print("Dictionary stored in UserDefaults.")
+        }
+    }
+    
+    internal func retrieveUserData(){
+        if let data = UserDefaults.standard.data(forKey: "User") {
+                user = try? decoder.decode(SubscriberModel.self, from: data)
+        } else {
+                debugPrint("User not found in UserDefaults.")
+            }
+    }
+    
+    //MARK: - CreateNewSubscriber API -
+    public func formSubmit(param: RHSubscriber){
+        WEB_SER.api_POST(endPoint: subscribers, param: param.toDictionary())
+        { [self] (result,data) in
+            switch result{
+                case .success(let response):
+                    self.storeUserData(response: data)
+                    self.retrieveUserData()
+                    delegate?.didReceiveAPIResponse(response, "Create new subscriber")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "Create new subscriber")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - Subscriber Detail API -
+    public func getSubscriberDetail(){
+        WEB_SER.api_GET(endPoint: subscribers + "/\(user?.data?.id ?? "")")
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    self.storeUserData(response: data)
+                    self.retrieveUserData()
+                    delegate?.didReceiveAPIResponse(response, "SubscriberDetail")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "SubscriberDetail")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - Delete Subscriber API -
+    public func DeleteSubscriber(){
+        WEB_SER.api_DELETE(endPoint: subscribers + "/\(user?.data?.id ?? "")", param: [:])
+        { [self] (result) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "DeleteReferral")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "DeleteReferral")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - Update Subscriber API -
+    public func UpdateSubscriber(param: RHSubscriber){
+        WEB_SER.api_PATCH(endPoint: subscribers + "/\(user?.data?.id ?? "")", param: param.toDictionary())
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    self.storeUserData(response: data)
+                    self.retrieveUserData()
+                    delegate?.didReceiveAPIResponse(response, "UpdateSubscriber")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "UpdateSubscriber")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - Click Capture API -
+    public func clickCapture(social: String){
+        WEB_SER.api_POST(endPoint: subscribers + "/\(user?.data?.id ?? "")" + "/click_capture", param: ["social" : social])
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "clickCapture")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "clickCapture")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - GetMyReferrals API -
+    public func getMyReferrals(){
+        WEB_SER.api_GET(endPoint: subscribers + "/\(user?.data?.id ?? "")" + "/referrals_data")
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "getMyReferrals")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "getMyReferrals")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - GetLeaderboard API -
+    public func getLeaderboard(){
+        WEB_SER.api_GET(endPoint: leaderboard)
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "getLeaderboard")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "getLeaderboard")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - Track Referral API -
+    public func trackReferral(param: RHTrackReferral){
+        WEB_SER.api_POST(endPoint: subscribers + "/track_referral_conversion_event", param: param.toDictionary())
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "TrackReferral")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "TrackReferral")
+                    print(err)
+            }
+        }
+    }
+    //MARK: - Confirm Referral API -
+    public func ConfirmReferral(){
+        WEB_SER.api_POST(endPoint: subscribers + "/\(user?.data?.id ?? "")" + "/confirm", param: [:])
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "Confirm referral")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "Confirm referral")
+                    print(err)
+            }
+        }
+    }
+    
+    //MARK: - Organic Track Referral API -
+    public func OrganicTrackReferral(param: RHOrganicReferral){
+        WEB_SER.api_POST(endPoint: subscribers + "/organic_track_referral", param: param.toDictionary())
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "Organic Track Referral")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "Organic Track Referral")
+                    print(err)
+            }
+        }
+    }
+    
+    //MARK: - Create Pending Referral API -
+    public func CreatePendingReferral(param: RHReferral){
+        WEB_SER.api_POST(endPoint: subscribers + "/pending_referral", param: param.toDictionary())
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "Create Pending Referral")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "Create Pending Referral")
+                    print(err)
+            }
+        }
+    }
+
+    //MARK: - Referral List API -
+    public func RewardsList(){
+        WEB_SER.api_GET(endPoint: subscribers + "/\(user?.data?.id ?? "")" + "/rewards")
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "Rewards List")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "Rewards List")
+                    print(err)
+            }
+        }
+    }
+
+    //MARK: - Referrer List API -
+    public func ReferrerList(){
+        WEB_SER.api_GET(endPoint: subscribers + "/referrer?os_type=ios&device=mobile&ip_address=\(RHApiKey.IP)&screen_size=\(RHApiKey.SCREEN_SIZE)")
+        { [self] (result, data) in
+            switch result{
+                case .success(let response):
+                    delegate?.didReceiveAPIResponse(response, "Referrer List")
+                    print(response)
+                case .failure(let err):
+                    delegate?.didFailWithError(err, "Referrer List")
+                    print(err)
+            }
+        }
+    }
+}
+
+//    //MARK: - Create Visitor Referral API -
+//    public func CreateVisitorReferral(){
+//        WEB_SER.api_POST_V2(endPoint: subscribers + "/visitor_referral", param: [:])
+//        { [self] (result, data) in
+//            switch result{
+//                case .success(let response):
+//                    delegate?.didReceiveAPIResponse(response, "Create Visitor Referral")
+//                    print(response)
+//                case .failure(let err):
+//                    delegate?.didFailWithError(err, "Create Visitor Referral")
+//                    print(err)
+//            }
+//        }
+//    }
+
+////MARK: - Promote Referral API -
+//
+//func Promote(){
+//    WEB_SER.api_POST(endPoint: subscribers + "/\(user?.data?.id ?? "")" + "/promote", param: [:])
+//    { [self] (result, data) in
+//        switch result{
+//            case .success(let response):
+//                delegate?.didReceiveAPIResponse(response, "Promote")
+//                print(response)
+//            case .failure(let err):
+//                delegate?.didFailWithError(err, "Promote")
+//
+//                print(err)
+//        }
+//    }
+//}
+////MARK:- CreateNewSubscriber API
+//public func CreateNewSubscriber(param: paraCreateNewSubscriber,completion: @escaping ([String: Any]?, Bool, String) -> ()){
+//    WEB_SER.api_POST(task: create_new_subscribers, param: param.toDictionary())
+//    { [self] (result) in
+//        switch result{
+//            case .success(let response):
+//                completion(response, true, "Object parsed")
+//                delegate?.didReceiveAPIResponse(response)
+//                print(response)
+//            case .failure(let err):
+//                completion(nil, false, "Something went wrong")
+//                delegate?.didFailWithError(err)
+//                print(err)
+//        }
+//    }
+//}
---
+++ Sources/ReferralHero-iOS/APIManager/RequestModel.swift
@@ -0,0 +1,228 @@
+//
+//  RequestParameterModel.swift
+//  ReferralHero
+//
+//  Created by YESHA PATEL on 17/05/2023.
+//
+
+//MARK: - Track Referral -
+
+public struct RHTrackReferral{
+    public var email: String
+    public var name: String
+    public init(email: String, name: String) {
+        self.email = email
+        self.name = name
+    }
+    
+    func toDictionary() -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["email"] = email
+        dictionary["name"] = name
+        return dictionary
+    }
+}
+public struct RHOrganicReferral{
+    public var email: String
+    public var name: String
+    public var referrer: String?
+    public var hosting_url: String?
+    
+    public init(email: String, name: String, referrer: String? = nil, hosting_url: String? = nil) {
+        self.email = email
+        self.name = name
+        self.referrer = referrer
+        self.hosting_url = hosting_url
+    }
+    
+    func toDictionary() -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["email"] = email
+        dictionary["name"] = name
+        dictionary["referrer"] = referrer
+        dictionary["hosting_url"] = hosting_url
+        return dictionary
+    }
+}
+public struct RHReferral{
+    public var email: String
+    public var name: String
+    public var referrer: String?
+    public var hosting_url: String?
+    
+    public init(email: String, name: String, referrer: String? = nil, hosting_url: String? = nil) {
+        self.email = email
+        self.name = name
+        self.referrer = referrer
+        self.hosting_url = hosting_url
+    }
+    
+    func toDictionary() -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["email"] = email
+        dictionary["name"] = name
+        dictionary["referrer"] = referrer
+        dictionary["hosting_url"] = hosting_url
+        dictionary["os_type"] = "IOS"
+        dictionary["ip_address"] = RHApiKey.IP
+        dictionary["screen_size"] = RHApiKey.SCREEN_SIZE
+        dictionary["device"] = RHApiKey.Device
+        return dictionary
+    }
+}
+//MARK: - Create subscriber -
+
+public struct RHSubscriber{
+    public var email: String
+    public var domain: String
+    public var name: String
+    public var phone_number: String?
+    public var ip_address: String?
+    public var os_type: String?
+    public var screen_size: String?
+    public var extra_field: String?
+    public var extra_field2: String?
+    public var status: String?
+    public var referrer: String?
+    public var points: String?
+    public var double_optin: String?
+    public var source: String?
+    public var device: String?
+    public var conversion_value: String?
+    public var conversion_category: String?
+    public var transaction_id: String?
+    public var crypto_wallet_address: String?
+    
+    // Usage
+    
+    public init(email: String, domain: String, name: String, phone_number: String? = nil, ip_address: String? = nil, os_type: String? = nil, screen_size: String? = nil, extra_field: String? = nil, extra_field2: String? = nil, status: String? = nil, referrer: String? = nil, points: String? = nil, double_optin: String? = nil, source: String? = nil, device: String? = nil, conversion_value: String? = nil, conversion_category: String? = nil, transaction_id: String? = nil, crypto_wallet_address: String? = nil) {
+        self.email = email
+        self.domain = domain
+        self.name = name
+        self.phone_number = phone_number
+        self.ip_address = ip_address
+        self.os_type = os_type
+        self.screen_size = screen_size
+        self.extra_field = extra_field
+        self.extra_field2 = extra_field2
+        self.status = status
+        self.referrer = referrer
+        self.points = points
+        self.double_optin = double_optin
+        self.source = source
+        self.device = device
+        self.conversion_value = conversion_value
+        self.conversion_category = conversion_category
+        self.transaction_id = transaction_id
+        self.crypto_wallet_address = crypto_wallet_address
+        
+    }
+    
+    func toDictionary() -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["email"] = email
+        dictionary["domain"] = domain
+        dictionary["name"] = name
+        dictionary["phone_number"] = phone_number
+        dictionary["extra_field"] = extra_field
+        dictionary["extra_field2"] = extra_field2
+        dictionary["status"] = status
+        dictionary["referrer"] = referrer
+        dictionary["points"] = points
+        dictionary["double_optin"] = double_optin
+        dictionary["source"] = source
+        dictionary["device"] = device
+        dictionary["conversion_value"] = conversion_value
+        dictionary["conversion_category"] = conversion_category
+        dictionary["ip_address"] = RHApiKey.IP
+        dictionary["device"] = RHApiKey.Device
+        dictionary["os_type"] = "IOS"
+        dictionary["screen_size"] = RHApiKey.SCREEN_SIZE
+        dictionary["crypto_wallet_address"] = crypto_wallet_address
+        
+        print("-----------------------------")
+        print(dictionary)
+        print("-----------------------------")
+        return dictionary
+    }
+}
+//MARK: - Subscriber Model -
+
+struct SubscriberModel : Codable {
+    let status : String?
+    let data : SubscriberData?
+    let calls_left : String?
+    let timestamp : Int?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case status = "status"
+        case data = "data"
+        case calls_left = "calls_left"
+        case timestamp = "timestamp"
+    }
+}
+struct SubscriberData : Codable {
+    let id : String?
+    let name : String?
+    let email : String?
+    let phone_number : String?
+    let crypto_wallet_address : String?
+    let crypto_wallet_provider : String?
+    let extra_field : String?
+    let extra_field_2 : String?
+    let option_field : String?
+    let conversion_amount : Double?
+    let code : String?
+    let position : Int?
+    let referred : Bool?
+    let referred_by : Referred_by?
+    let people_referred : Int?
+    let promoted : Bool?
+    let promoted_at : String?
+    let verified : Bool?
+    let verified_at : Int?
+    let points : Int?
+    let risk_level : Int?
+    let host : String?
+    let source : String?
+    let device : String?
+    let referral_link : String?
+    let created_at : Int?
+    let last_updated_at : Int?
+    let response : String?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id = "id"
+        case name = "name"
+        case email = "email"
+        case phone_number = "phone_number"
+        case crypto_wallet_address = "crypto_wallet_address"
+        case crypto_wallet_provider = "crypto_wallet_provider"
+        case extra_field = "extra_field"
+        case extra_field_2 = "extra_field_2"
+        case option_field = "option_field"
+        case conversion_amount = "conversion_amount"
+        case code = "code"
+        case position = "position"
+        case referred = "referred"
+        case referred_by = "referred_by"
+        case people_referred = "people_referred"
+        case promoted = "promoted"
+        case promoted_at = "promoted_at"
+        case verified = "verified"
+        case verified_at = "verified_at"
+        case points = "points"
+        case risk_level = "risk_level"
+        case host = "host"
+        case source = "source"
+        case device = "device"
+        case referral_link = "referral_link"
+        case created_at = "created_at"
+        case last_updated_at = "last_updated_at"
+        case response = "response"
+    }
+}
+struct Referred_by : Codable {
+}
