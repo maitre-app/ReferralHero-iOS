c9d9c1a46890fd611d2ae345177cbd2739f76d99
Prashant Aggarwal <paggarwal@payfare.com>
2025-07-26 17:10:50 +0530

    branch.io implemented

--- Package.swift
+++ Package.swift
@@ -6,18 +6,22 @@ import PackageDescription
 let package = Package(
     name: "ReferralHero-iOS",
     platforms: [
-            .iOS(.v13)
-        ],
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ReferralHero-iOS",
             targets: ["ReferralHero-iOS"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/BranchMetrics/ios-branch-deep-linking-attribution.git", from: "2.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ReferralHero-iOS"),
+            name: "ReferralHero-iOS",
+            dependencies: [.product(name: "BranchSDK", package: "ios-branch-deep-linking-attribution")]),
     ]
 )
